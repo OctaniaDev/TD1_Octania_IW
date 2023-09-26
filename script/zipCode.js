8172f6374198232a5cc2c82144d3987b7720b5b4
Flavien <flavien.louvet2004@gmail.com>
2023-09-26 12:49:14 +0200

    feat : html input bar zipCode #2

---
+++ script/zipCode.js
@@ -0,0 +1,35 @@
+let zipCode = document.getElementById('zipCodeInput');
+let cityName = document.getElementById('cityNameSelect');
+
+// zipCodeHttpRequest();
+
+// LISTENER
+zipCode.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      zipCodeHttpRequest();
+      console.log(zipCode.value);
+    }
+});
+
+
+
+/**
+ * Use geo.api to return all city for an zip code and display it in the console
+ */
+function zipCodeHttpRequest(){
+    let url = 'https://geo.api.gouv.fr/communes?codePostal=' + zipCode.value;
+    fetch(url)
+    .then(reponse => {
+        if(!reponse.ok){
+            throw new Error('Error something went wrong');
+        } return reponse.json();
+    })
+    .then(data => {
+        for(let i = 0; i <= data.length-1; i++){
+            console.log(data[i].nom + " - " + data[i].code)
+        }
+    })
+    .catch(error =>{
+        console.error('Error : ', error);
+    });
+}
