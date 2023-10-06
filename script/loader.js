175576e78b789edfb0a5683dd32a3461e6dfd0d1
FI$$ <FISSSSSS@proton.me>
2023-10-06 18:15:43 +0200

    fix : display loader #25

--- script/loader.js
+++ script/loader.js
@@ -1,19 +1,24 @@
-// window.addEventListener('load', function () {
-//     setTimeout(function () {
-//         hideLoader();
-//         showButtonAndFooter();
-//     }, 2000);
-// });
+window.addEventListener('load', async function () {
+    hideContent();
+    await setTimeout(function () {
+        hideLoader();
+        showContent();
+    }, 2000);
+});
 
-// function hideLoader() {
-//     const loaderContainer = document.querySelector('.loader-container');
-//     loaderContainer.style.display = 'none';
-// }
+function hideLoader() {
+    const loaderContainer = document.querySelector('.loader-container');
+    loaderContainer.style.display = 'none';
+}
 
-// function showButtonAndFooter() {
-//     const button = document.getElementById('open-modal-btn');
-//     const footer = document.querySelector('.footer-general');
+function hideContent() {
+    document.querySelector('header').style.display = 'none'
+    document.querySelector('main').style.display = 'none'
+    document.querySelector('footer').style.display = 'none'
+}
 
-//     button.classList.remove('hidden');
-//     footer.classList.remove('hidden');
-// }
+function showContent() {
+    document.querySelector('header').style.display = ''
+    document.querySelector('main').style.display = ''
+    document.querySelector('footer').style.display = ''
+}
